0ea959401f089ad88d6b23008a0b0d0383e0aaa9
naruhitotakei <naruhito.takei@gmail.com>
2021-08-28 00:00:13 +0600

    'added'

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
 export default function AboutPage() {
   return (
-    <div className="grid md:grid-cols-2 gap-6 grid-cols-1">
+    <div className='grid md:grid-cols-2 gap-6 grid-cols-1'>
       <div>
-        <section className="mb-6">
-          <h2 className="mb-3 text-xl font-bold">What is Tailwind?</h2>
+        <section className='mb-6'>
+          <h2 className='mb-3 text-xl font-bold'>What is Tailwind?</h2>
 
           <p>
             Tailwind CSS is a highly customizable, low-level CSS framework that
@@ -16,7 +16,7 @@ export default function AboutPage() {
         </section>
 
         <section>
-          <h2 className="mb-3 text-xl font-bold">What is Next.js?</h2>
+          <h2 className='mb-3 text-xl font-bold'>What is Next.js?</h2>
           <p>
             Next.js is a minimalistic framework for creating server-rendered
             React applications.
@@ -25,11 +25,11 @@ export default function AboutPage() {
       </div>
 
       <Image
-        alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
-        src="/critter.svg"
+        alt='A one-eyed alien holding a broken cable connected between a server and a desktop computer'
+        src='/critter.svg'
         width={476}
         height={297.17}
       />
     </div>
-  );
+  )
 }
